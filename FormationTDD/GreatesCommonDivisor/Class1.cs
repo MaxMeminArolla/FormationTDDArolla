6ff3daec54c2664ebc0fae5747b0c4fd0032be03
MEMIN <maxmemin@hotmail.com>
2022-01-17 10:18:55 +0100

    creation project and test project with provided function

---
+++ FormationTDD/GreatesCommonDivisor/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreatesCommonDivisor
+{
+    public class Class1
+    {
+        public static int gcd(int a, int b)
+        {
+            return b == 0 ? a : gcd(b, a % b);
+        }
+    }
+}
