322908363c13a0ed7a3de6b6524599a39f250093
MEMIN <maxmemin@hotmail.com>
2022-01-19 15:51:54 +0100

    new kata Train Reservation and first ko test to obtain available seats

---
+++ FormationTDD/TrainReservation_Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using TrainReservation;
+
+namespace TrainReservation_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Should_get_next_train_available_seats()
+        {
+            Kata.get_next_train_AvailableSeats("Caen", "Paris", "11h58");
+        }
+    }
+}
